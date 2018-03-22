11a85f5b3ebc9e64f7647f01fb3c87a0c0ab2111
Fredrik Teschke <f@ftes.de>
2018-03-22 20:12:38 +0100

    [doc] document allowed es version for configuration.js

--- config/configuration.js
+++ config/configuration.js
@@ -1,6 +1,8 @@
 /**
  * NOTICE: this file is intentionally outside of `src` (override via `--configDir`)
  *
+ * You can restricted to the EcmaScript version supported by node.js (https://nodejs.org/en/docs/es6/).
+ *
  * Add additional responses or modify the static responses read from the `data` directory.
  * If a matching file was found, the parsed JSON is available in `res.body`.
  */
