ddf64e5be98f0e5b793db8b74200eaf2cf3d2a09
Fredrik Teschke <fteschke@wescale.com>
2018-08-23 16:42:35 +0200

    [fix] do not fail if no pre-configuration script is available
    - the script should be optional

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unpublished] - date
 
+## [1.2.1] - 2018-08-23
+### Fixed
+- Do not fail if no pre-configuration script is available (it should be optional)
+
 ## [1.2.0] - 2018-08-03
 ### Added
 - Dockerize
--- src/index.js
+++ src/index.js
@@ -34,12 +34,14 @@ if (!allMandatoryOptionsGiven) {
 
 const app = express();
 
-const preConfiguration = __non_webpack_require__(path.resolve(path.join(options.configDir, 'pre-configuration')));
-if (preConfiguration) {
+try {
+  const preConfiguration = __non_webpack_require__(path.resolve(path.join(options.configDir, 'pre-configuration')));
   preConfiguration(app, {
     options,
     middleware,
   });
+} catch (e) {
+  console.info('No pre-configuration file found.');
 }
 
 app.use(morgan('tiny')); // log requests
