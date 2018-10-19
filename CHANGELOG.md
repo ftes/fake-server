43d5fb12076a2a8c9709fd1eb8f1c4d93fba60a7
Fredrik Teschke <fteschke@wescale.com>
2018-10-19 08:40:02 +0200

    [fix] skip proxy middleware if res.body is already present

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,9 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
-## [Unpublished] - date
+## [1.2.2] - 2018-10-19
+### Fixed
+- Skip proxy middleware if `res.body` is already present
 
 ## [1.2.1] - 2018-08-23
 ### Fixed
--- src/middleware/proxy-middleware.js
+++ src/middleware/proxy-middleware.js
@@ -3,23 +3,37 @@ import record from './record';
 
 const base64 = str => Buffer.from(str).toString('base64');
 
-const proxyAndRecord = (host, cliOptions, { recordData = true } = {}) => proxy(host, {
-  proxyReqPathResolver: req => req.originalUrl,
-  userResDecorator: (proxyRes, proxyResData, userReq) => {
-    if (recordData) {
-      record(userReq, proxyResData, cliOptions, proxyRes.statusCode);
+const proxyAndRecord = (
+  host, cliOptions,
+  { recordData = true, proxyReqPathResolver } = {},
+) => {
+  const proxyMiddleware = proxy(host, {
+    proxyReqPathResolver: proxyReqPathResolver || (req => req.originalUrl),
+    userResDecorator: (proxyRes, proxyResData, userReq) => {
+      if (recordData) {
+        record(userReq, proxyResData, cliOptions, proxyRes.statusCode);
+      }
+
+      return proxyResData;
+    },
+    proxyReqOptDecorator: (proxyReqOpts) => {
+      if (cliOptions.basicAuthUser && cliOptions.basicAuthPassword) {
+        const basicAuthToken = base64(`${cliOptions.basicAuthUser}:${cliOptions.basicAuthPassword}`);
+        // eslint-disable-next-line no-param-reassign
+        proxyReqOpts.headers.Authorization = `Basic ${basicAuthToken}`;
+      }
+      return proxyReqOpts;
+    },
+  });
+
+  return (req, res, next, ...args) => {
+    if (res.body) {
+      next();
+      return;
     }
 
-    return proxyResData;
-  },
-  proxyReqOptDecorator: (proxyReqOpts) => {
-    if (cliOptions.basicAuthUser && cliOptions.basicAuthPassword) {
-      const basicAuthToken = base64(`${cliOptions.basicAuthUser}:${cliOptions.basicAuthPassword}`);
-      // eslint-disable-next-line no-param-reassign
-      proxyReqOpts.headers.Authorization = `Basic ${basicAuthToken}`;
-    }
-    return proxyReqOpts;
-  },
-});
+    proxyMiddleware(req, res, next, ...args);
+  };
+};
 
 export default proxyAndRecord;
